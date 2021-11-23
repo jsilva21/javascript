93cc4290bc300edf539f3589b4e3ede80a350ad9
Jhonnes da Silva <jhonnes.silva21@hotmail.com>
2021-11-23 13:18:43 -0300

    ex016

---
+++ exercicios/aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+function contar(){
+    var inicio = document.getElementById('txtini')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var res = document.getElementsById('res')
+
+    if(inicio.value.length ==0 || fim.value.length ==0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        
+    }
+}
