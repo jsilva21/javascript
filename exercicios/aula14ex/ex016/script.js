8d08d849bcb5e84923d0c302fab2b3743c97b5dd
Jhonnes da Silva <jhonnes.silva21@hotmail.com>
2021-11-23 14:47:45 -0300

    Atualização

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,19 +1,28 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+function contar(){
+    var ini= document.getElementById('txti')
+    var fim= document.getElementById('txtf')
+    var passo= document.getElementById('txtp')
+    var res= document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
-    }else{
-        res.innerHTML= 'Contando:'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[Erro] Faltam dados!!!')
+    } else{
+        res.innerHTML = 'Contando: <br>'
 
-        let i = Number(ini.value)
-        let f = Number(ini.value)
-        let p = Number(passo.value)
-
-        for(let c=i; c<=f; c+=p){
-            res.innerHTML += `${c}`
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f){
+            //Contagem crescente
+        for(var c=i; c<=f; c+=p){
+            res.innerHTML+= `${c} \u{1F449} `
+            }
+        }else{
+            //Contagem regressiva
+            for(var c=i; c>=f; c-= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
+        res.innerHTML += `\u{1F3C1}`
     }
+}
